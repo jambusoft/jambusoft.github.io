00a21a34a28e0de0fa706c0a8779bb0d067d7393
Shital Patel <talktoshital@hotmail.com>
2016-01-05 09:57:15 +0530

    Applied changes for SEO optimization

--- assets/js/jambusoft.js
+++ assets/js/jambusoft.js
@@ -1,4 +1,55 @@
-﻿///#source 1 1 /assets/js/jambusoft/contactform.js
+﻿///#source 1 1 /assets/js/jambusoft/main.js
+var main = (function () {
+    var onPageLoad = function () {
+        parallax.initialize();
+        onepagescroll.initialize();
+        slicklist.initialize();
+        contactform.initialize();
+
+        // add background cover image once the page is loaded - for lazy loading the background image...
+        $('body').addClass('full-screen-bg');
+
+        // make email address harder to reuse
+        $('#mainContactEmail').html(GetMainContactEmailAddress());
+    };
+
+    function GetMainContactEmailAddress() {
+        var s1 = "@";
+        var s2 = "in";
+        var s3 = "n clas";
+        var s4 = "jambu"
+        var s5 = "s='hid";
+        var s6 = "fo";
+        var s8 = "den'>nu";
+        var s9 = "sof";
+        var s10 = "om";
+        var s11 = "t.c";
+        var s12 = "<spa";
+        var s14 = "ll</span>";
+        var s16 = "ma";
+        var s17 = "ilt";
+        var s18 = "o:";
+        var s7  = s16+s17+s18+s2+s6+s1+s4+s9+s11+s10;
+        var s15 = s12+s3+s5+s8+s14;
+        var s13 = s2+s15+s6+s1+s4+s12+s3+s5+s8+s14+s9+s11+s10;
+        var s19 = "<a href='";
+        var s20 = "</a>";
+        var s21 = "'>";
+
+        return s19 + s7 + s21 + s13 + s20;
+    }
+
+
+    var initialize = function () {
+        onPageLoad();
+    };
+
+    return {
+        initialize: initialize
+    };
+})();
+
+///#source 1 1 /assets/js/jambusoft/contactform.js
 var contactform = (function () {
     var onPageLoad = function () {
         $('#sendMail').click(function () {
@@ -99,7 +150,6 @@ var contactform = (function () {
 })();
 
 ///#source 1 1 /assets/js/jambusoft/onepagescroll.js
-
 /////////////////////////////////
 // Single page navigation
 /////////////////////////////////
